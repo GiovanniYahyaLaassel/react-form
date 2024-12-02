c657662ea33f6e4bc481125903978812ed7550d5
Laassel <lassel.giovanni@gmail.com>
2024-12-02 17:38:15 +0100

    ADD  function to manage the submit

--- src/components/AddPostForm.jsx
+++ src/components/AddPostForm.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-function AddPostForm() {
+function AddPostForm(addNewPost) {
     const [newTitle, setNewTitle] = useState('')  //tengo traccia del'input
 
     //funzione per gestire il cambiamento dell'input
@@ -9,16 +9,28 @@ function AddPostForm() {
         console.log('valore aggiornato', event.target.value);
     }
 
+    // funzione per gestire il submit
+    const  handleSubmit = (event) => {
+        // evito il refresh
+        event.preventDefault();
+
+        if (newTitle.trim() !== "") { // rimuovo gli spazi bianchi nella stringa 
+            addNewPost(newTitle); 
+            setNewTitle(""); // resetto  il campo
+        }
+    }
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <input 
             type="text"
             value={newTitle}
             onChange={handleInputChange}
             placeholder="Inserisci il titolo del nuovo posts "
             />
+            <button type="submit">Aggiungi Post</button>
         </form>
     );
 }
 
- export default AddPostForm;
+export default AddPostForm;
